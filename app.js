75157b0fe12cce0b6f45e0cdca12db36bebabe1b
Esteban Cortina <estebannsebastian@gmail.com>
2022-08-26 13:13:06 -0500

    Class

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ let b=6;
 
 //Deja de usar let, no es necesario :p
 
-c = a+b;
+//for([<expresion>]) una expresion es opcional
+//for(i=1,j=2,k=3;i<=10,j>=3,k=3;i++,j+=2,k++) podemos inicializar e incrementar varias variables 
+
+
